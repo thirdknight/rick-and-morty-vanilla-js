30331a84227a4be034fea1375766c7d60be78537
Edinson <edinson.ningles@gmail.com>
2021-11-25 18:32:18 +0100

    adding code for rick and morty

--- src/component.js
+++
@@ -1,9 +0,0 @@
-// import { join } from "lodash-es";
-
-export function component() {
-  const element = document.createElement("div");
-
-  element.innerHTML = join(["Hello", "webpack", "amigo"], " ");
-
-  return element;
-}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import { component } from "./component";
-import { logo } from "./logo";
-// import "./app.css";
-import "./app.scss";
-document.body.appendChild(component());
-document.body.appendChild(logo());
+import API from './js/api.js'
+import Character from './js/character.js'
+import './index.scss'
+const api = new API()
+let currentCaracter = 4
+const $loadNext = document.querySelector('#load-next')
+
+$loadNext.addEventListener('click', async () => {
+  const characterData = await api.getCharacter(++currentCaracter)
+  new Character(characterData)
+})
+
+async function initApp(initCharacterId) {
+  const characterData = await api.getCharacter(initCharacterId)
+  console.log(characterData)
+  new Character(characterData)
+}
+
+initApp(currentCaracter)
